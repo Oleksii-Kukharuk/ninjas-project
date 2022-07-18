1e279767662d083ebf1c890d5be59eb3fd3d1421
InnaSolod <solodinna6@gmail.com>
2022-07-18 11:37:49 +0300

    save

--- src/index.js
+++ src/index.js
@@ -1,16 +1,45 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
-     const bodyOverscroll = document.querySelector('body');
-  
-    menuBtnRef.addEventListener("click", () => {
-      const expanded =
-        menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-  
-      menuBtnRef.classList.toggle("is-open");
-      menuBtnRef.setAttribute("aria-expanded", !expanded);
-  
-      mobileMenuRef.classList.toggle("is-open");
-      bodyOverscroll.classList.toggle('modal-open');
-    });
-  })();
+  const menuBtnRef = document.querySelector('[data-menu-button]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+
+  menuBtnRef.addEventListener('click', () => {
+    const expanded =
+
+      menuBtnRef.classList.toggle('--is-open');
+
+
+    mobileMenuRef.classList.toggle('--is-open');
+    bodyOverscroll.classList.toggle('modal-open');
+  });
+})();
+
+(() => {
+  const menuBtnRefsec = document.querySelector('[data-menu-button-sec]');
+  const mobileMenuRef = document.querySelector('[data-menu]');
+  const mobileMenuRefss = document.querySelector('[return]');
+  const mobileMenuRefsss = document.querySelector('[return-sec]');
+
+
+  menuBtnRefsec.addEventListener('click', () => {
+
+    mobileMenuRef.classList.toggle('--is-open');
+    mobileMenuRefss.classList.toggle('visually-hidden');
+    mobileMenuRefsss.classList.toggle('visually-hidden');
+    bodyOverscroll.classList.toggle('modal-open');
+  });
+})();
+
+(() => {
+  const btn = document.querySelector('[visually-hidden-frst]');
+  const mobileMenuRef = document.querySelector('[visually-hidden-scnd]');
+
+
+  btn.addEventListener('click', () => {
+
+
+    btn.classList.toggle('visually-hidden');
+
+    mobileMenuRef.classList.toggle('visually-hidden');
+    bodyOverscroll.classList.toggle('modal-open');
+  });
+})();

715f76dfa86b3f672ba8d6aadc52d853731119c2
Oleksii-Kukharuk <104523043+Oleksii-Kukharuk@users.noreply.github.com>
2022-07-18 12:51:40 +0000

    Deploying to gh-pages from @ Oleksii-Kukharuk/ninjas-project@be2eb1864c736e6779274b717c792b1b39ab5640 🚀

--- index.65f24848.js
+++
@@ -1,2 +0,0 @@
-!function(){var e,t,a;e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]"),a=document.querySelector("body"),e.addEventListener("click",(function(){var n="true"===e.getAttribute("aria-expanded")||!1;e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!n),t.classList.toggle("is-open"),a.classList.toggle("modal-open")}))}();
-//# sourceMappingURL=index.65f24848.js.map
